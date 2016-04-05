8e433c3ecc83b133eb8fc27bcc42a40f56fa1850
天明 <tianming@naitang.com>
2016-04-05 13:27:23 +0800

    configure

--- common/api/src/main/java/cn/demo/service/dubbo/register/service/impl/RegisterServiceImpl.java
+++ common/api/src/main/java/cn/demo/service/dubbo/register/service/impl/RegisterServiceImpl.java
@@ -3,22 +3,22 @@
  * 
  * File Created at 2016年04月05日
  * 
- * Copyright 2016 naitang.com All right reserved. This software is the
- * confidential and proprietary information of naitang.com ("Confidential
+ * Copyright 2016 demo.com All right reserved. This software is the
+ * confidential and proprietary information of demo.com ("Confidential
  * Information"). You shall not disclose such Confidential Information and shall
  * use it only in accordance with the terms of the license agreement you entered
- * into with naitang.com .
+ * into with demo.com .
  */
 package cn.demo.service.dubbo.register.service.impl;
 
 import cn.demo.service.dubbo.register.service.RegisterService;
 
 /**
- * Created by TianMing <tianming@naitnag.com> on 16/4/5.
+ * Created by TianMing <tianming@demo.com> on 16/4/5.
  *
  * @Type RegisterServiceImpl
  * @Desc 普通类型转换
- * @Author TianMing <tianming@naitnag.com>
+ * @Author TianMing <tianming@demo.com>
  * @Date 2016-04-05
  * @Version V1.0
  */
